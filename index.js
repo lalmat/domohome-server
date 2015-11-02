5c30d2321ce4ab2702dcbdd4e71505b08a5466a6
Mathieu Lallemand <lallemand.mathieu@gmail.com>
2015-11-02 21:58:02 +0100

    chrore(*): adding CORS header to work as a node

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 var express = require ('express');
 var port    = 8080;
 var app     = express();
+var cors    = require('cors');
 
 var domo    = require ('./domotic.js');
-
 domo.config = {
   'door_lock': {pin:7, state:0, mode:'output'}
 }
 domo.init();
 
-app.get('/setItem', function (req, res) {
+app.get('/setItem', cors(), function (req, res) {
   var item  = req.query.item;
   var state = req.query.state % 2;
   domo.set(item, state, function() {
@@ -18,7 +18,7 @@ app.get('/setItem', function (req, res) {
   });
 });
 
-app.get('/getItem', function (req, res) {
+app.get('/getItem', cors(), function (req, res) {
   var item  = req.query.item;
   res.send(domo.config[item]);
 });
